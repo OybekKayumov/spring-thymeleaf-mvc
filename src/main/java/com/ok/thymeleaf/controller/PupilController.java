2289a641baab732175f9cbf7ad30bda80f2dcdd7
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-10-20 07:31:13 +0500

    Add radio button

--- src/main/java/com/ok/thymeleaf/controller/PupilController.java
+++ src/main/java/com/ok/thymeleaf/controller/PupilController.java
@@ -1,20 +1,29 @@
 package com.ok.thymeleaf.controller;
 
 import com.ok.thymeleaf.model.Pupil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class PupilController {
+
+    @Autowired
+    @Value("${cities}")
+    private List<String> cities;
 
     @GetMapping("/showPupilForm")
     public String showForm(Model model) {
         //Pupil pupil = new Pupil();
         //model.addAttribute("pupil", pupil);
         model.addAttribute("pupil", new Pupil());
+        model.addAttribute("cities", cities);
         return "pupil-form";
     }
 
