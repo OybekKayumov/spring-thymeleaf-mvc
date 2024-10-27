a219b0c9436c47c9a622373ed154f61def2f6369
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-10-27 14:07:49 +0500

    Add checkbox into pupil-form

--- src/main/java/com/ok/thymeleaf/model/Pupil.java
+++ src/main/java/com/ok/thymeleaf/model/Pupil.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,5 +16,6 @@ public class Pupil {
     private String sex;
     private String city;
     private String favoriteDrink;
+    private List<String> favoriteCars;
 
 }
